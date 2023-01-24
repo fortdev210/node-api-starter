977490444a2820dac4975cc698c7f6785e7b945d
fortdev210 <sharp.blader2050@gmail.com>
2023-01-25 01:22:15 +0800

    init setup

---
+++ prisma/migrations/20230124172128_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "is_email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Express.js + Typescript + Postgres + Prisma
+
+## Features
+
+- TypeScript, Postgres, Express, Prisma configuration
+- Basic api creation pattern
+- Add request validation using zod
+- Redis connection
+- Logger service setup
+- Jest testing
+- Email transaction using mailchimp mandrill
+- Payment setup using stripe
+- Twillio setup
+- Swagger for api documentation
+- JWT authentication
